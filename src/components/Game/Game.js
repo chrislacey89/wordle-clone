c79f8e520a23b02a5396e1057c3226c586865ba9
Chris Lacey <clacey@fusionalliance.com>
2023-03-09 19:51:50 -0500

    minor refactor

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -18,21 +18,7 @@ function Game() {
   const lastGuessHasBeenMade = guesses.length + 1 === NUM_OF_GUESSES_ALLOWED;
   function updateGuesses(guess) {
     setGuesses([...guesses, checkGuess(guess, answer)]);
-    setGuessedLetters((guessedLettersCopy) => {
-
-      for (let i = 0; i < guess.length; i++) {
-        if (!guessedLettersCopy[guess[i]]) {
-          guessedLettersCopy[guess[i]] = 'incorrect';
-        }
-        if(answer.includes(guess[i])){
-          guessedLettersCopy[guess[i]] = 'misplaced';
-        }
-        if (guess[i] === answer[i]){
-          guessedLettersCopy[guess[i]] = 'correct';
-        }
-      }
-      return guessedLettersCopy
-    });
+    setGuessedLetters(setLetters(guess));
     if (lastGuessHasBeenMade && guess !== answer) {
       setWinState("lose");
     }
@@ -40,10 +26,27 @@ function Game() {
       setWinState("win");
     }
   }
+  function setLetters(guess){
+    const guessedLettersCopy = {...guessedLetters};
+    for (let i = 0; i < guess.length; i++) {
+      if (!guessedLettersCopy[guess[i]]) {
+        guessedLettersCopy[guess[i]] = 'incorrect';
+      }
+      if(answer.includes(guess[i])){
+        guessedLettersCopy[guess[i]] = 'misplaced';
+      }
+      if (guess[i] === answer[i]){
+        guessedLettersCopy[guess[i]] = 'correct';
+      }
+    }
+    return guessedLettersCopy
+  }
+  
   function resetGame() {
     setGuesses([]);
     setWinState("running");
     setAnswer(sample(WORDS));
+    setGuessedLetters({})
   }
   return (
     <>
